0f1819bac90cd532fd419d77180c3e1692045164
Xada18 <111207031+Xada18@users.noreply.github.com>
2023-06-26 02:34:52 -0300

    Add files via upload

--- Practico 2 U5/app.py
+++ Practico 2 U5/app.py
@@ -44,23 +44,23 @@ def login():
 def funcion1():
     preceptor = Preceptor.query.filter_by(correo=session.get('preceptor')).first()
     cursos = Curso.query.filter_by(idpreceptor=preceptor.id).all()
-    return render_template('seleccionar_curso.html', preceptor=preceptor, cursos=cursos)
+    return render_template('seleccionar_curso1.html', preceptor=preceptor, cursos=cursos)
 
-@app.route('/seleccionar_curso', methods = ['GET', 'POST'])
-def seleccionarCurso():
+@app.route('/seleccionar_curso1', methods = ['GET', 'POST'])
+def seleccionarCurso1():
     if 'curso' not in request.form or not request.form['curso']:
         preceptor = Preceptor.query.filter_by(correo=session.get('preceptor')).first()
         cursos = Curso.query.filter_by(idpreceptor=preceptor.id).all()
-        return render_template('seleccionar_curso.html', preceptor=preceptor, cursos=cursos, error="Seleccione un curso")
+        return render_template('seleccionar_curso1.html', preceptor=preceptor, cursos=cursos, error="Seleccione un curso")
     else:
         idcurso = request.form['curso']
-        estudiantes = Estudiante.query.filter_by(idcurso=idcurso).all()
+        estudiantes = Estudiante.query.filter_by(idcurso=idcurso).order_by(Estudiante.nombre, Estudiante.apellido).all()
         return render_template('registrar_asistencia.html', estudiantes=estudiantes, idcurso=idcurso)
 
 @app.route('/registrar_asistencia', methods = ['GET', 'POST'])
 def registrarAsistencia():
     idcurso = request.form['idcurso']
-    estudiantes = Estudiante.query.filter_by(idcurso=idcurso).all()
+    estudiantes = Estudiante.query.filter_by(idcurso=idcurso).order_by(Estudiante.nombre, Estudiante.apellido).all()
     
     if 'fecha' not in request.form or 'clase' not in request.form or not request.form['fecha'] or not request.form['clase']:
         return render_template('registrar_asistencia.html', estudiantes=estudiantes, idcurso=idcurso, error="Faltan datos")
@@ -79,8 +79,10 @@ def registrarAsistencia():
 
                 just = str(estudiante.id) + "/justificacion"
 
-                if request.form[just]:
+                if just in request.form and request.form[just]:
                     justificacion=request.form[just]
+                else:
+                    justificacion=""
 
             asistencia = Asistencia(fecha=request.form['fecha'], codigoclase=request.form['clase'], asistio=asistio, justificacion=justificacion, idestudiante = estudiante.id)
 
@@ -95,7 +97,55 @@ def registrarAsistencia():
 
 @app.route('/func2')
 def funcion2():
-    return render_template('informe_con_detalles.html', cursos=[])
+    preceptor = Preceptor.query.filter_by(correo=session.get('preceptor')).first()
+    cursos = Curso.query.filter_by(idpreceptor=preceptor.id).all()
+    return render_template('seleccionar_curso2.html', preceptor=preceptor, cursos=cursos)
+
+@app.route('/seleccionar_curso2', methods = ['GET', 'POST'])
+def seleccionarCurso2():
+    if 'curso' not in request.form or not request.form['curso']:
+        preceptor = Preceptor.query.filter_by(correo=session.get('preceptor')).first()
+        cursos = Curso.query.filter_by(idpreceptor=preceptor.id).all()
+        return render_template('seleccionar_curso2.html', preceptor=preceptor, cursos=cursos, error="Seleccione un curso")
+    else:
+        idcurso = request.form['curso']
+        estudiantes = Estudiante.query.filter_by(idcurso=idcurso).order_by(Estudiante.nombre, Estudiante.apellido).all()
+
+        
+        contadores=[]
+
+        for estudiante in estudiantes:
+            asistencias = Asistencia.query.filter_by(idestudiante=estudiante.id).all()
+
+            asists1 = 0
+            asists2 = 0
+            faltas1 = 0
+            faltas2 = 0
+            justificadas1 = 0
+            justificadas2 = 0
+            
+
+            for asistencia in asistencias:
+                if asistencia.asistio == "s":
+                    if asistencia.codigoclase == 1:
+                        asists1 +=1
+                    elif asistencia.codigoclase == 2:
+                        asists2 +=1
+                elif asistencia.asistio == "n":
+                    if asistencia.codigoclase == 1:
+                        faltas1 +=1
+                        if asistencia.justificacion:
+                            justificadas1+=1
+                    elif asistencia.codigoclase == 2:
+                        faltas2 +=1
+                        if asistencia.justificacion:
+                            justificadas2+=1
+            
+            conts=[asists1, asists2, faltas1, faltas2, justificadas1, justificadas2]
+
+            contadores.append(conts)
+
+        return render_template('informe_con_detalles.html', estudiantes=estudiantes, idcurso=idcurso, contadores=contadores)
 
 @app.route('/informe_con_detalles')
 def informeConDetalles():
